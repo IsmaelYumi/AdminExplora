21379c9713b2e9eee228ef4665b7d6689a49fabe
Ismael <93888943+IsmaelYumi@users.noreply.github.com>
2025-08-11 16:53:20 -0500

    Inicio

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+       <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600 mb-4">
+        ¡Hola desde Tailwind! 🚀
+      </h1>
+      <button className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700 transition">
+        Click aquí
+      </button>
+    </div>
+       
+    </>
+  )
+}
+
+export default App
