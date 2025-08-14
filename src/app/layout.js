fdcdd0757d7d8d0a0c9c2399eae922c43321ce51
leodamac <leodamac@espol.edu.ec>
2025-08-14 08:17:12 -0500

    Menu principal implementado

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
-import './globals.css';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+import "./globals.css";
+
+
 export const metadata = {
   title: 'Admin Explora',
   description: 'Admin panel for managing Explora content',
@@ -7,8 +8,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="es">
+      <body >{children}</body>
     </html>
   )
 }
